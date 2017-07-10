1f1f1fd14c600f276b78d72a731d8508d1585bd4
Łukasz Sack <lukaszsack@ukaszs-MacBook-Pro.local>
2017-07-10 12:21:37 +0200

    One class only, code cleared.

--- Object_Test/Browser.py
+++ Object_Test/Browser.py
@@ -1,33 +1,28 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-class go_to_page(object):
-    url = None
 
-    def __init__(self, driver):
-        self.driver = driver
-        #driver = webdriver.Chrome(executable_path='/Users/lukaszsack/downloads/chromedriver')
-        driver.implicitly_wait(15)
-
+class LoginPage():
     find_login = (By.NAME, "email")
     find_password = (By.NAME, "password")
     find_login_button = (By.XPATH, "//button [@class = 'btn btn-primary btn-block ladda-button fadeIn animated']")
 
-    def navigate(self):
-        self.driver.get(self.url)
-
-class Page_Login(go_to_page):
+    def __init__(self, driver):
+        self.driver = driver
 
     def insert_email(self):
         email = "admin@phptravels.com"
-        ins_mail = self.driver.find_element(*go_to_page.find_login)
+        ins_mail = self.driver.find_element(*LoginPage.find_login)
         ins_mail.send_keys(email)
 
     def insert_password(self):
         password = "demoadmin"
-        ins_pass = self.driver.find_element(*go_to_page.find_password)
+        ins_pass = self.driver.find_element(*LoginPage.find_password)
         ins_pass.send_keys(password)
 
     def login_button_click(self):
-        button_click = self.driver.find_element(*go_to_page.find_login_button)
+        wait = WebDriverWait(self.driver, 10)
+        button_click = self.driver.find_element(*LoginPage.find_login_button)
         button_click.click()
+        wait.until(EC.presence_of_element_located((By.XPATH, "//a [@class = 'navbar-brand']")))
--- Object_Test/Test_Case.py
+++ Object_Test/Test_Case.py
@@ -1,27 +1,28 @@
 import unittest
 from selenium import webdriver
 from Object_Test import Browser
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 class LoginSucces(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path='/Users/lukaszsack/downloads/chromedriver')
+        self.browser.implicitly_wait(15)
 
     def test_login(self):
-        browser = self.browser
-        browser.get("http://www.phptravels.net/admin")
+        self.browser.get("http://www.phptravels.net/admin")
 
-        login_page = Browser.Page_Login(self.browser)
+        login_page = Browser.LoginPage(self.browser)
         login_page.insert_email()
         login_page.insert_password()
         login_page.login_button_click()
 
-        time.sleep(4)
+        assert "Dashboard" in self.browser.title
+
+        self.browser.save_screenshot('login_succes.png')
+
     def tearDown(self):
         self.browser.close()
+
 
 if __name__ == '__main__':
     unittest.main()
