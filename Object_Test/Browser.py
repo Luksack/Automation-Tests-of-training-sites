a111bd08175d18e9fc6f1aa17b7c8be53d3569ab
Łukasz Sack <lukaszsack@ukaszs-MacBook-Pro.local>
2017-07-11 13:33:46 +0200

    login fail added, asserts

--- Object_Test/Browser.py
+++ Object_Test/Browser.py
@@ -3,26 +3,38 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
+
 class LoginPage():
     find_login = (By.NAME, "email")
     find_password = (By.NAME, "password")
     find_login_button = (By.XPATH, "//button [@class = 'btn btn-primary btn-block ladda-button fadeIn animated']")
+    find_fail_msg = (By.XPATH, "//div [@class = 'alert alert-danger loading wow fadeIn animated animated']")
 
     def __init__(self, driver):
         self.driver = driver
 
-    def insert_email(self):
-        email = "admin@phptravels.com"
+    def insert_email(self, email):
         ins_mail = self.driver.find_element(*LoginPage.find_login)
         ins_mail.send_keys(email)
 
-    def insert_password(self):
-        password = "demoadmin"
+    def insert_password(self, password):
         ins_pass = self.driver.find_element(*LoginPage.find_password)
         ins_pass.send_keys(password)
 
-    def login_button_click(self):
+    def login_success_button_click(self):
         wait = WebDriverWait(self.driver, 10)
         button_click = self.driver.find_element(*LoginPage.find_login_button)
         button_click.click()
         wait.until(EC.presence_of_element_located((By.XPATH, "//a [@class = 'navbar-brand']")))
+
+    def login_fail_button_click(self):
+        wait = WebDriverWait(self.driver, 10)
+        button_click = self.driver.find_element(*LoginPage.find_login_button)
+        button_click.click()
+        wait.until(EC.visibility_of_element_located
+                   ((By.XPATH,
+                        "//div [@class = 'alert alert-danger loading wow fadeIn animated animated']")))
+
+    def login_empty_button_click(self):
+        button_click = self.driver.find_element(*LoginPage.find_login_button)
+        button_click.click()
--- Object_Test/Test_Case.py
+++ Object_Test/Test_Case.py
@@ -1,28 +1,70 @@
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 from Object_Test import Browser
 
 
 class LoginSucces(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome(executable_path='/Users/lukaszsack/downloads/chromedriver')
-        self.browser.implicitly_wait(15)
+        self.driver = webdriver.Chrome(executable_path='/Users/lukaszsack/downloads/chromedriver')
+        self.driver.implicitly_wait(15)
 
-    def test_login(self):
-        self.browser.get("http://www.phptravels.net/admin")
+    def test_login_success(self):
+        self.driver.get("http://www.phptravels.net/admin")
 
-        login_page = Browser.LoginPage(self.browser)
-        login_page.insert_email()
-        login_page.insert_password()
-        login_page.login_button_click()
+        login_page = Browser.LoginPage(self.driver)
+        login_page.insert_email("admin@phptravels.com")
+        login_page.insert_password("demoadmin")
+        login_page.login_success_button_click()
 
-        assert "Dashboard" in self.browser.title
+        assert "Dashboard" in self.driver.title
 
-        self.browser.save_screenshot('login_succes.png')
+        self.driver.save_screenshot('login_succes.png')
+
+    def test_wrong_password(self):
+        self.driver.get("http://www.phptravels.net/admin")
+
+        login_page = Browser.LoginPage(self.driver)
+        login_page.insert_email("admin@phptravels.com")
+        login_page.insert_password("12345")
+        login_page.login_fail_button_click()
+
+        error_message = self.driver.find_element\
+            (By.XPATH,
+                    "//div [@class = 'alert alert-danger loading wow fadeIn animated animated']")
+        self.assertEquals(error_message.text, "Invalid Login Credentials")
+
+        self.driver.save_screenshot("Login_password_fail.png")
+
+    def test_wrong_email(self):
+        self.driver.get("http://www.phptravels.net/admin")
+
+        login_page = Browser.LoginPage(self.driver)
+        login_page.insert_email("wrong@email.com")
+        login_page.insert_password("demoadmin")
+        login_page.login_fail_button_click()
+
+        error_message = self.driver.find_element\
+            (By.XPATH,
+                    "//div [@class = 'alert alert-danger loading wow fadeIn animated animated']")
+        self.assertEquals(error_message.text, "Invalid Login Credentials")
+
+    def test_empty_fields(self):
+        self.driver.get("http://www.phptravels.net/admin")
+
+        login_page = Browser.LoginPage(self.driver)
+        login_page.insert_email("")
+        login_page.insert_password("")
+        login_page.login_empty_button_click()
+
+        assert "dashboard" not in self.driver.title
+
+        self.driver.save_screenshot("empty_validation.png")
 
     def tearDown(self):
-        self.browser.close()
-
+        self.driver.close()
 
 if __name__ == '__main__':
     unittest.main()
